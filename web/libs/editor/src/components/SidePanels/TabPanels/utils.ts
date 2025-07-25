c4cad01e22d7c235073e9cc520297c244e43e254
shiwei2007 <shiwei2007@gmail.com>
2025-07-25 15:21:54 +0800

    Update utils.ts
    remove region and relation tabs

--- web/libs/editor/src/components/SidePanels/TabPanels/utils.ts
+++ web/libs/editor/src/components/SidePanels/TabPanels/utils.ts
@@ -245,9 +245,12 @@ export const restorePanel = (showComments: boolean): StoredPanelState => {
   // don't use comments tab anywhere if it's disabled
   const countOfAllAvailableTabs = panelViews.length - (showComments ? 0 : 1);
 
+  const outdatedState = allTabs && allTabs.some((view) => ["regions", "relations"].includes(view.name));
+
   // stored state can have less tabs than available, for example if it was stored on old version
-  // or if comments were enabled; then return default state
-  if (!allTabs || allTabs.length !== countOfAllAvailableTabs) {
+  // or if comments were enabled, or if outdated tabs like Regions or Relations are present;
+  // then return default state
+  if (!allTabs || allTabs.length !== countOfAllAvailableTabs || outdatedState) {
     const defaultPanel = showComments ? enterprisePanelDefault : openSourcePanelDefault;
 
     return { panelData: defaultPanel, collapsedSide: defaultCollapsedSide };
